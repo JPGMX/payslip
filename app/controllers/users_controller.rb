18829792f0e8746bd430382a81f564132700bffb
JP <gonjuanp@gmail.com>
2018-08-12 19:33:39 -0500

    Added control and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users=User.all
   end
 
   def import
+    User.import(params[:file].path)
+    redirect_to users_index_path, notice: 'File Imported'
   end
 end
